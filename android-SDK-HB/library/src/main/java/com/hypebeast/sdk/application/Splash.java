8884bbb2d1328c750a715b39be01cafa6bd56ed4
世外桃源 <jobhesk@gmail.com>
2015-11-25 18:55:52 +0800

    temp update
    not fully fix the permission issue

--- android-SDK-HB/library/src/main/java/com/hypebeast/sdk/application/Splash.java
+++ android-SDK-HB/library/src/main/java/com/hypebeast/sdk/application/Splash.java
@@ -5,26 +5,34 @@ import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+
+import com.greysonparrelli.permiso.Permiso;
 
 /**
  * Created by hesk on 18/11/15.
  */
 public abstract class Splash extends AppCompatActivity {
 
-    public final static int PERMISSIONS_REQUEST_EXTERNAL_STORAGE = 123;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Permiso.getInstance().setActivity(this);
         this.setContentView(getLayoutId());
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             V23permission_request();
         } else {
             synchronizeData();
         }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Permiso.getInstance().setActivity(this);
     }
 
     protected abstract void onPermissionGranted();
@@ -36,69 +44,39 @@ public abstract class Splash extends AppCompatActivity {
     @LayoutRes
     protected abstract int getLayoutId();
 
-
     protected void V23permission_request() {
-        // Here, thisActivity is the current activity
-        if (ContextCompat.checkSelfPermission(this,
-                Manifest.permission.WRITE_EXTERNAL_STORAGE)
-                != PackageManager.PERMISSION_GRANTED
-                ||
-                ContextCompat.checkSelfPermission(this,
-                        Manifest.permission.READ_EXTERNAL_STORAGE)
-                        != PackageManager.PERMISSION_GRANTED
+        Permiso.getInstance().requestPermissions(
+                new Permiso.IOnPermissionResult() {
+                    @Override
+                    public void onPermissionResult(Permiso.ResultSet resultSet) {
+                        if (resultSet.areAllPermissionsGranted()) {
+                            // Permission granted!
+                            onPermissionGranted();
+                        } else {
+                            // Permission denied.
+                            onPermissionDenied();
+                        }
+                    }
 
-                ) {
+                    @Override
+                    public void onRationaleRequested(Permiso.IOnRationaleProvided callback, String... permissions) {
+                        Permiso.getInstance().showRationaleInDialog("Request permissions", permission_message(), null, callback);
+                    }
+                },
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.INTERNET,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
 
-            // Should we show an explanation?
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
-                    Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
-                // Show an expanation to the user *asynchronously* -- don't block
-                // this thread waiting for the user's response! After the user
-                // sees the explanation, try again to request the permission.
-            } else {
-                // No explanation needed, we can request the permission.
-                ActivityCompat.requestPermissions(this,
-                        new String[]{
-                                Manifest.permission.WRITE_EXTERNAL_STORAGE,
-                                Manifest.permission.READ_EXTERNAL_STORAGE
-                        },
-                        PERMISSIONS_REQUEST_EXTERNAL_STORAGE);
-
-                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
-                // app-defined int constant. The callback method gets the
-                // result of the request.
-            }
-        } else {
-            synchronizeData();
-        }
+        );
     }
 
+    protected String permission_message() {
+        return "You need to grant those permissions";
+    }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
-        switch (requestCode) {
-            case PERMISSIONS_REQUEST_EXTERNAL_STORAGE: {
-                // If request is cancelled, the result arrays are empty.
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-
-                    // permission was granted, yay! Do the
-                    // contacts-related task you need to do.
-
-                    onPermissionGranted();
-
-                } else {
-
-                    // permission denied, boo! Disable the
-                    // functionality that depends on this permission.
-                    onPermissionDenied();
-                }
-                return;
-            }
-
-            // other 'case' lines to check for other
-            // permissions this app might request
-        }
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        Permiso.getInstance().onRequestPermissionResult(requestCode, permissions, grantResults);
     }
-
 }
